bea1b6aefb2acbf5255762e3cb7ad535e4f752b4
vitoria2469 <113610328+vitoria2469@users.noreply.github.com>
2025-09-29 11:59:19 -0300

    Delete projeto-agis-main directory

--- projeto-agis-main/src/main/java/com/fatec/agis/persistence/DaoSimples.java
+++
@@ -1,9 +0,0 @@
-package com.fatec.agis.persistence;
-
-import java.sql.SQLException;
-import java.util.List;
-
-public interface DaoSimples<E>{
-
-	public List<E> listar() throws SQLException, ClassNotFoundException;
-}
